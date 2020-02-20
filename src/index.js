96c03d361387e8e5171315c0f343ead741847a44
DzianisBogdan <thespirit3000@gmail.com>
2020-02-20 21:36:25 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length > 0) {
+    return array.sort((x, y) => x-y)[0];
+  };
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length > 0) {
+    return array.sort((x, y) => y-x)[0];
+  };
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length > 0) {
+    return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0)/array.length;
+  };
   return 0;
 }
